c6993e9695e529d7917e4a36e3b1815a459dec31
joliuu <juliooorodriguessss@gmail.com>
2024-06-03 14:09:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function addFilme() {   
+    var titulo = document.getElementById('titulo').value
+    var diretor = document.getElementById('diretor').value
+    var anoLancamento = document.getElementById('ano').value
+    var lista = document.getElementById('lista')
+
+    class Filme {
+        constructor(tit, dir , an) {
+            this.tit = tit
+            this.dir = dir
+            this.an = an
+        }
+
+        dadosFilme() {
+            return `${this.tit} - ${this.dir} (${this.an})<br><br>`
+        }
+    }
+
+    if(titulo === "" || diretor === "" || isNaN(anoLancamento)) {
+        alert('Não deixe nada vazio')
+    } else {
+        let novoFilme = new Filme(titulo, diretor, anoLancamento)
+        let dadosDoFilme = novoFilme.dadosFilme()
+        lista.innerHTML = lista.innerHTML + dadosDoFilme
+    }
+}
+
