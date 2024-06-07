e6ece0eddad7e2c1990ca38196555884e019fbfb
joliuu <juliooorodriguessss@gmail.com>
2024-06-07 10:06:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 function addFilme() {   
-    var titulo = document.getElementById('titulo').value
-    var diretor = document.getElementById('diretor').value
-    var anoLancamento = document.getElementById('ano').value
-    var lista = document.getElementById('lista')
+    var titulo = document.getElementById('titulo').value;
+    var diretor = document.getElementById('diretor').value;
+    var anoLancamento = document.getElementById('ano').value;
+    var lista = document.getElementById('lista');
 
     class Filme {
         constructor(tit, dir , an) {
-            this.tit = tit
-            this.dir = dir
-            this.an = an
+            this.tit = tit;
+            this.dir = dir;
+            this.an = an;
         }
 
         dadosFilme() {
@@ -18,11 +18,11 @@ function addFilme() {
     }
 
     if(titulo === "" || diretor === "" || isNaN(anoLancamento)) {
-        alert('Não deixe nada vazio')
+        alert('Não deixe nada vazio');
     } else {
-        let novoFilme = new Filme(titulo, diretor, anoLancamento)
-        let dadosDoFilme = novoFilme.dadosFilme()
-        lista.innerHTML = lista.innerHTML + dadosDoFilme
+        let novoFilme = new Filme(titulo, diretor, anoLancamento);;
+        let dadosDoFilme = novoFilme.dadosFilme();
+        lista.innerHTML = lista.innerHTML + dadosDoFilme;
     }
 }
 
